066b93a5b9c264b30d825517dd04122f0f7030b2
Wellington-Santos <wellington.september@gmail.com>
2025-04-11 04:44:49 -0300

    feat: Realizando ajustes na aplicação para consultar as ações e salvar

--- main.py
+++ main.py
@@ -4,14 +4,20 @@ from src.utils.helpers import setup_logger
 def main():
     logger = setup_logger("main_logger")
 
-    tickers = ["ITUB4", "PETR4.SA", "VALE3.SA", "CESP6.SA", "KLBN11.SA", "TAEE11.SA"]
+    # Lista de ações que serão consultadas
+    tickers = ["ITUB4", "PETR4", "VALE3", "CESP6", "KLBN11", "TAEE11"]
     logger.info("Iniciando coleta de dados do Yahoo Finance...")
 
     try:
-        df = fetch_stock_data(tickers)
-        logger.info(f"{len(df)} registros coletados com sucesso.")
-        print(df.head())
-        save_raw_data(df)
+        data = fetch_stock_data(tickers)
+        logger.info(f"Dados coletados para {len(data)} ações.")
+
+        # Mostra uma previa dos dados coletados
+        sample_ticker = "PETR4.SA"
+        if sample_ticker in data:
+            print(f"\nPrévia dos dados para {sample_ticker}:\n", data[sample_ticker].head())
+
+        save_raw_data(data)
         logger.info("Dados salvo com sucesso.")
     except Exception as e:
         logger.error(f"Erro ao executar a ingestão: {e}")
--- src/ingestion/fetch_data.py
+++ src/ingestion/fetch_data.py
@@ -1,20 +1,43 @@
+import os
 import yfinance as yf
 import pandas as pd
 
 from datetime import datetime
+from src.utils.helpers import setup_logger
+
+logger = setup_logger(__name__)
 
 def fetch_stock_data(tickers, start = "2020-01-01", end=None):
 
     if end is None:
         end = datetime.today().strftime('%Y-%m-%d')
-        
+
+    logger.info(f"Iniciando a coleta de dados de {len(tickers)} ações de {start} até {end}")
     data = {}
+    
     for ticker in tickers:
-        print(f"Fetching data for {ticker}")
-        df = yf.download(f"{ticker}.SA", start = start, end = end)
-        data[ticker] = df
+        try:
+            logger.info(f"Baixando dados para {ticker}")
+            df = yf.download(f"{ticker}.SA", start = start, end = end)
+            
+            if df.empty:
+                logger.warning(f"Nenhum dado retornado para {ticker}")
+            else:
+                data[ticker] = df
+                logger.info(f"{ticker} - {len(df)} registros obtidos")
+
+        except Exception as e:
+            logger.error(f"Erro ao coletar dados de {ticker}: {e}")
     return data
 
 def save_raw_data(data, output_dir = "data/raw"):
+    os.makedirs(output_dir, exist_ok=True)
+
     for ticker, df in data.items():
-        df.to_csv(f"{output_dir}/{ticker}.csv")
+        try:
+            file_path = os.path.join(output_dir, f"{ticker}.csv")
+            df.to_csv(file_path)
+
+            logger.info(f"Dados brutos salvos em {file_path}")
+        except Exception as e:
+            logger.error(f"Erro ao salvar dados de {ticker}: {e}")
